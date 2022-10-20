9a82574811eac5428a3cba163170a6f96a9b1720
IKEH1024 <pondau1024@gmail.com>
2022-10-20 15:27:32 +0900

    create demo project

---
+++ GitHubPlayground/GitHubPlayground/GitHubPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPlaygroundApp.swift
+//  GitHubPlayground
+//
+//  Created by HIROKI IKEUCHI on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

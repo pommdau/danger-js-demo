fde7750af382edfeffc89687fa5326b5702dab6c
IKEH1024 <pondau1024@gmail.com>
2022-10-27 15:02:33 +0900

    update dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,29 +1,58 @@
 let isAllCheckPassed = true;
 
+///////////////////
+// タイトルのチェック
+///////////////////
+
+// PRのタイトルに[WIP]が含まれる場合は、作業中なのでfailさせる
 if (danger.github.pr.title.includes('[WIP]')) {
   fail("Should NOT inclued 'WIP' in your PR title");
 }
 
-// if (!danger.github.pr.assignee) {
-//   warn("Should select PR reviewer");
-//   isAllCheckPassed = false;
-// }
-
-const hasIssuesNumber = /#[0-9]/.test(danger.github.pr.title);
+// PRのタイトルに課題番号が含まれているかどうか(DEV_1-XXX)
+const hasIssuesNumber = /DEV_1-[0-9]/.test(danger.github.pr.title);
 if (!hasIssuesNumber) {
   warn("Should include issues number in your PR title");
   isAllCheckPassed = false;
 }
 
+//////////////////
+// PR設定のチェック
+//////////////////
+
+// PRにレビュアーが指定されているかどうか
+if (!danger.github.pr.reviews) {
+  warn("Should select PR reviewer");
+  isAllCheckPassed = false;
+}
+
+// PRにassigneesが指定されているかどうか
+if (!danger.github.pr.assignee) {
+  warn("Should select PR assignees");
+  isAllCheckPassed = false;
+}
+
+/////////////////
+// 変更量のチェック
+/////////////////
+
+// 500行以上の追加・削除の変更があったかどうか
 const diffSize = Math.max(danger.github.pr.additions, danger.github.pr.deletions);
 if (diffSize > 500) {
   warn("Should reduce diffs less than 500");
   isAllCheckPassed = false;
 }
 
+// 変更したファイルの数が10より多いかどうか
 if (danger.github.pr.changed_files > 10 ) {
   warn("Should reduce change files less than 10");
   isAllCheckPassed = false;
 }
 
-if (isAllCheckPassed) markdown('## All checkes have passed :tada:')
+/////////////////
+// 全体の結果の出力
+/////////////////
+
+if (isAllCheckPassed) {
+  markdown('## All checkes have passed :tada:')
+}
